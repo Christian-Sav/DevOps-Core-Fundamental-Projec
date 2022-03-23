dec4248ad5109a2ab63fbee4ee697847e39abf58
Christian-Sav <chris.sav@outlook.com>
2022-03-23 10:48:51 +0000

    Updated test.sh

--- flask-app/test.sh
+++ flask-app/test.sh
@@ -3,4 +3,6 @@ sudo apt update && sudo apt -y install python3 python3-pip python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+pip3 install wheel
+python setup.py bdist_wheel
 python3 -m pytest --cov=Project --cov-report=html
